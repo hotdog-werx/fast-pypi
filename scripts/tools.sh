bca99147e72b4355509bd95b6e7cff69b386aceb
James Trousdale <james.trousdale@protonmail.com>
2025-06-21 11:48:41 -0500

    Fix tool versions

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
-uv tool install "ruff>=0.11.13,<1"
+uv tool install "ruff==0.11.13"
 
-uv tool install "poethepoet>=0.35.0,<1"
+uv tool install "poethepoet==0.35.0"
 
 # For testing
-uv tool install "poetry"
-uv tool install "pip"
+uv tool install "poetry==2.1.3"
+uv tool install "pip==25.1.1"
